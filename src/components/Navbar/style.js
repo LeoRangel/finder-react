cabeb2df38d681e02935c431c2897ec8744e62da
8bitsL <51348224+8bitsL@users.noreply.github.com>
2022-06-30 18:48:14 -0300

    Menu completo

--- src/components/Navbar/style.js
+++ src/components/Navbar/style.js
@@ -4,10 +4,35 @@ export const NavContent = styled.nav`
   color: white;
   padding: 2rem 0;
   margin-bottom: 7.2rem;
+   
 
   .container {
     width: 100%;
     display: flex;
     align-items: center;
+	justify-content: space-between;
+  },
+
+  .navMenu{
+	display: flex;
+	align-items: center;
+  }
+
+  .alignMenu{
+	display: flex;
   }
 `;
+
+export const UlItem = styled.ul`
+	display: flex;
+	font-weight: 300;
+
+	li{
+		margin: 0 25px 0 0;
+		cursor: pointer;
+		&:nth-child(1) {
+			margin-right: 3rem;
+			margin-left: 5.8rem;
+		}
+	}
+`
