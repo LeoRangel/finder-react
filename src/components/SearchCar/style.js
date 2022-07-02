e681ab6f7a37733cd13380dccd5cbe37be3be1ae
LeoRangel <leandrorangelm@gmail.com>
2022-07-02 12:37:20 -0300

    Ajustes de responsividade em Header e SearchCar

--- src/components/SearchCar/style.js
+++ src/components/SearchCar/style.js
@@ -1,76 +1,84 @@
 import styled from "styled-components";
 
-export const SearchCar = styled.nav`
-	display: flex;
-	flex-direction: column;
-	margin-bottom: 8rem;
-	width: 100%;
-	gap: 2rem;
+export const SearchCar = styled.form`
+  .container {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 8rem;
+    width: 100%;
+    gap: 2rem;
+  }
 
-	button{
-		height: 100%;
-		width: 15rem;
-		padding: 1.4rem 3.2rem;
-	}
+  #formSearchCar {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    border-radius: 8px;
+    padding: 1rem;
+    gap: 1rem;
+    border: 1px solid rgba(255, 255, 255, 0.12);
+    background: rgba(255, 255, 255, 0.04);
 
-	#formSearchCar {
-		display: flex;
-		align-items: center;
-		width: 100%;
-		border-radius: 8px;
-		padding: 0.8rem 1rem;
-		gap: 3rem;
-		border: 1px solid rgba(255, 255, 255, 0.12);
-		background: rgba(255, 255, 255, 0.04);
-	
-	}
-	
-	#formSearchCar input,
-	select {
-		background: transparent;
-		height: 6rem;
-		border: none;
-		color: var(--gray-700);
-	}
-	
-	#formSearchCar input:focus,
-	select:focus {
-		box-shadow: 0 0 0 0;
-		outline: 0;
-	}
-	
-	.labelFormat {
-		display: flex;
-		align-items: center;
-	}
-	
-	.labelFormat i {
-		background: transparent;
-		color: var(--gray-700);
-		/* margin: 0; */
-		padding: 0 10px;
-	}
-	
-	#searchInput{
-		color: var(--white) !important;
-		max-width: 22rem;
-	}
-	
-	.selectHeader {
-		padding-right: 1rem;
-		cursor: pointer;
-	}
-	
-	.selectHeader option {
-		background: var(--gray-900);
-		color: var(--white);
-		
-	}
-	
-	.searchSeparator {
-		display: flex;
-		height: 4rem;
-		opacity: 0.15;
-		border: 0.5px solid #FFFFFF;
-	}
-`
+    @media (min-width: 1000px) {
+      padding-left: 2rem;
+    }
+
+    @media (max-width: 1000px) {
+      padding: 1.5rem;
+    }
+
+    label {
+      width: 100%;
+      display: flex;
+      align-items: center;
+
+      :not(:last-of-type) {
+        @media (min-width: 1000px) {
+          border-right: 0.5px solid rgba(255, 255, 255, 0.15);
+          padding-right: 1rem;
+          margin-right: 1rem;
+        }
+
+        @media (max-width: 1000px) {
+          border-bottom: 0.5px solid rgba(255, 255, 255, 0.15);
+          padding-bottom: 1rem;
+        }
+      }
+
+      i {
+        background: transparent;
+        color: var(--gray-500);
+      }
+    }
+
+    input,
+    select {
+      /* width: 100%; */
+      background-color: transparent;
+      border: none;
+      color: var(--gray-500);
+    }
+
+    input {
+      @media (min-width: 1000px) {
+        width: 22rem;
+      }
+    }
+
+    button {
+      height: 100%;
+      padding: 1.4rem 3.2rem;
+
+      @media (max-width: 1000px) {
+        width: 100%;
+      }
+    }
+  }
+
+  @media (max-width: 1000px) {
+    #formSearchCar {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+`;
