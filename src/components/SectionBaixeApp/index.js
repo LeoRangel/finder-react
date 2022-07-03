be9db16426491cb9d20279a32688790265b3029a
thaysouza <tayanesilvasouzas@gmail.com>
2022-07-03 14:46:10 -0300

    criando a section - baixe app

---
+++ src/components/SectionBaixeApp/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import * as S from "./style";
+
+const SectioBaixeApp = ({}) => {
+  return (
+    <S.SectionBaixeApp>
+      <div className="container">
+        <div>Adicione aqui o conteúdo da section...</div>
+      </div>
+    </S.SectionBaixeApp>
+  );
+};
+
+export default SectioBaixeApp;
